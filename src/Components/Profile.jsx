ad8e8d48286146b0d648ec3178b691b045696beb
AleksandrBoguckiy <vagula91@gmail.com>
2020-09-04 15:05:20 +0700

    13 lesson git-repositiry

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <img src='https://www.onetwotrip.com/ru/blog/wp-content/uploads/2020/02/bali.jpg'/>
+        <div>
+            <img src='https://reallycom.ru/wp-content/uploads/2019/09/minecraft_youtube_avatar__speedycraft_by_bustedgun-d6ktkgl-e1567631489917.jpg'/>
+        </div>
+        <div>
+            My posts
+            <div>
+                New pospts
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>;
+}
+
+export default Profile;
