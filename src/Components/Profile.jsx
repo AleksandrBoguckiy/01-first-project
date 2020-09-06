bd45ef0b121d3dd2d06ddbcfb62bfdc42632cec4
AleksandrBoguckiy <vagula91@gmail.com>
2020-09-07 00:14:32 +0700

    14 lesson css-modules

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={s.content}>
         <img src='https://www.onetwotrip.com/ru/blog/wp-content/uploads/2020/02/bali.jpg'/>
         <div>
-            <img src='https://reallycom.ru/wp-content/uploads/2019/09/minecraft_youtube_avatar__speedycraft_by_bustedgun-d6ktkgl-e1567631489917.jpg'/>
+            <img
+                src='https://reallycom.ru/wp-content/uploads/2019/09/minecraft_youtube_avatar__speedycraft_by_bustedgun-d6ktkgl-e1567631489917.jpg'/>
         </div>
         <div>
             My posts
             <div>
-                New pospts
+                New posts
             </div>
-            <div>
-                Post 1
-            </div>
-            <div>
-                Post 2
+            <div className='posts'>
+                <div className={s.item}>
+                    Post 1
+                </div>
+                <div className={s.item}>
+                    Post 2
+                </div>
             </div>
         </div>
     </div>;
