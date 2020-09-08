5847609bd33b10b6fc470f528e8e478cbb662286
AleksandrBoguckiy <vagula91@gmail.com>
2020-09-08 23:20:43 +0700

    19 lesson route, browser-router, routing

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -5,10 +5,9 @@ import MyPosts from "./MyPosts/MyPosts";
 const Profile = () => {
     return (
         <div>
-            <img src='https://www.onetwotrip.com/ru/blog/wp-content/uploads/2020/02/bali.jpg'/>
+            <img src='https://www.onetwotrip.com/ru/blog/wp-content/uploads/2020/02/bali.jpg' />
             <div>
-                <img
-                    src='https://reallycom.ru/wp-content/uploads/2019/09/minecraft_youtube_avatar__speedycraft_by_bustedgun-d6ktkgl-e1567631489917.jpg'/>
+                <img src='https://reallycom.ru/wp-content/uploads/2019/09/minecraft_youtube_avatar__speedycraft_by_bustedgun-d6ktkgl-e1567631489917.jpg' />
             </div>
             <MyPosts/>
         </div>
