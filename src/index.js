59eca32ea95aa3da971419b904154b820548693a
AleksandBoguckiy <551015Santito551015>
2020-09-23 16:38:12 +0700

    29 lesson (упаковываем данные в state)

--- src/index.js
+++ src/index.js
@@ -3,37 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import Post from "./Components/Profile/MyPosts/Post/Post";
-import DialogItem from "./Components/Dialogs/DialogItem/DialogItem";
-import Message from "./Components/Dialogs/Message/Message";
-
-let posts = [
-    {id: '1', message: 'Hey, how are you?', like: '15'},
-    {id: '2', message: "It's my first post!", like: '23'},
-];
-
-let dialogs = [
-    {id: '1', name: 'Oleg'},
-    {id: '2', name: 'Egor'},
-    {id: '3', name: 'Mariya'},
-    {id: '4', name: 'Aleksandr'},
-    {id: '5', name: 'Ruslan'},
-    {id: '6', name: 'Irina'}
-];
-
-let messages = [
-    {id: '1', message: 'Hi!'},
-    {id: '2', message: 'How are you?'},
-    {id: '3', message: 'Hey, are you there?'}
-];
+import state from './redux/state';
 
 ReactDOM.render(
 
   <React.StrictMode>
-    <App posts={posts} dialogs={dialogs} messages={messages} />
+    <App state={state} />
   </React.StrictMode>,
   document.getElementById('root')
-
 
 );
 
---
+++ src/redux/state.js
@@ -0,0 +1,25 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: '1', message: 'Hey, how are you?', like: '15'},
+            {id: '2', message: "It's my first post!", like: '23'},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: '1', name: 'Oleg'},
+            {id: '2', name: 'Egor'},
+            {id: '3', name: 'Mariya'},
+            {id: '4', name: 'Aleksandr'},
+            {id: '5', name: 'Ruslan'},
+            {id: '6', name: 'Irina'}
+        ],
+        messages: [
+            {id: '1', message: 'Hi!'},
+            {id: '2', message: 'How are you?'},
+            {id: '3', message: 'Hey, are you there?'}
+        ]
+    }
+}
+
+export default state;
