45b82bacb86e3c06da157eae626b5048d046ab45
AleksandrBoguckiy <vagula91@gmail.com>
2021-03-17 21:00:56 +0700

    53 lesson (Классовый компонент (extends React.Component))

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,22 +3,19 @@ import style from './Users.module.css';
 import *as axios from "axios";
 import userPhoto from "../../assets/images/siluet_4108.jpg"
 
-const Users = (props) => {
+class Users extends React.Component {
 
-    const getUsers = () => {
-        if (props.users.length === 0) {
-
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
-            });
-        }
+    constructor(props) {
+        super(props)
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
-
-    return <div className={style.users}>
-        <button className={style.btn + " " + style.btn1} onClick={getUsers}>Get Users</button>
-        <h2>Users</h2>
-        {
-            props.users.map(u => <div className={style.wrapper} key={u.id}>
+    render() {
+        return <div className={style.users}>
+            <h2>Users</h2>
+            {
+                this.props.users.map(u => <div className={style.wrapper} key={u.id}>
                 <span>
                     <div className={style.avatar}>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} alt='Avatar'/>
@@ -26,14 +23,14 @@ const Users = (props) => {
                     <div className={style.divbtn}>
                         {u.followed
                             ? <button className={style.btn + " " + style.btn1} onClick={() => {
-                                props.unFollow(u.id)
+                                this.props.unFollow(u.id)
                             }}>Unfollow</button>
                             : <button className={style.btn + " " + style.btn1} onClick={() => {
-                                props.follow(u.id)
+                                this.props.follow(u.id)
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span className={style.data}>
+                    <span className={style.data}>
                     <span className={style.data1}>
                         <div>{u.name}</div>
                         <div>{"u.status"}</div>
@@ -43,8 +40,11 @@ const Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
+
 export default Users;
+
